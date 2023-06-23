b30bb3b18bb9613ac568afa041f232dbef92c626
TetiPalii <TetianaPalii9491@gmail.com>
2023-06-23 09:27:31 +0200

    fizing pagination adding loader

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,11 +4,13 @@ import { SearchBar } from 'components/searchbar/Searchbar';
 import { fetchImages } from 'helpers/fetchImages';
 import { Component } from 'react';
 import css from './App.module.css';
+import { Audio } from 'react-loader-spinner';
 
 export class App extends Component {
   state = {
     value: '',
     page: 1,
+    totalPages: 0,
     images: [],
     isLoading: false,
     error: null,
@@ -19,40 +21,56 @@ export class App extends Component {
   };
 
   componentDidUpdate(_, prevState) {
-    if (prevState.value !== this.state.value) {
+    if (
+      prevState.value !== this.state.value ||
+      prevState.page !== this.state.page
+    ) {
+      this.setState({ isLoading: true });
       fetchImages(this.state.value, this.state.page)
-        .then(({ hits }) =>
-          this.setState({ images: [...hits], isLoading: false })
-        )
-        .catch(error => console.log(error));
-    }
-    if (prevState.page !== this.state.page) {
-      // console.log(prevState);
-      // console.log(this.state);
-      console.log(this.state.page);
-      fetchImages(this.state.value, this.state.page) //
-        .then(({ hits }) => {
-          console.log(hits);
-          this.setState({ images: [...prevState.images, ...hits] });
+        .then(({ hits, totalHits }) => {
+          const totalPages = Math.floor(totalHits / 12);
+          this.setState({ totalPages });
+
+          if (this.state.page !== prevState.page) {
+            this.setState(prevState => {
+              return {
+                images: [...prevState.images, ...hits],
+                isLoading: false,
+              };
+            });
+          } else {
+            this.setState({ images: hits, isLoading: false });
+          }
         })
-        .catch(error => console.log(error));
+        .catch(error => this.setState({ error }));
     }
-    console.log(this.state.images);
   }
 
   onLoadMoreClick = e => {
-    // console.log(e);
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-    // console.log(this.state.page);
+    this.setState(prevState => ({ page: prevState.page + 40 }));
   };
+
   render() {
     return (
       <div className={css.App}>
         <SearchBar onSubmit={this.getSubmitValue} />
-        <ImageGallery images={this.state.images} />
-        {this.state.images.length > 0 && (
-          <Button onBtnClick={this.onLoadMoreClick} />
+        {this.state.error && <p> {this.state.error}</p>}
+        {this.state.isLoading && (
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="three-dots-loading"
+            wrapperStyle
+            wrapperClass
+          />
         )}
+        <ImageGallery images={this.state.images} />
+        {this.state.images.length > 0 &&
+          this.state.page !== this.state.totalPages && (
+            <Button onBtnClick={this.onLoadMoreClick} />
+          )}
       </div>
     );
   }
--- src/helpers/fetchImages.js
+++ src/helpers/fetchImages.js
@@ -3,9 +3,9 @@ const API_KEY = 'key=35862234-c36df0b3c5d22090eb9ac9504';
 // const page = 1;
 
 export const fetchImages = async (value, page) => {
-  console.log(page);
+  // console.log(page);
   const response = await fetch(
-    `${BASE_URL}?q=${value}&${page}&${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    `${BASE_URL}?q=${value}&page=${page}&${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
   if (!response.ok) {
     throw new Error(response.status);
